aedb999a9d43077fd4fb8f64f6969bf7683c47a5
Albert Zhang <aalbertzhang@gmail.com>
2021-03-11 15:55:02 -0800

    add axios

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
+axios.defaults.withCredentials = true;
+const temp =  () => {
+  test();
+  return <div>hi</div>;
+};
 
-const temp = () => <div>hello world</div>;
+const test = async () => {
+  const res = await axios.post('http://localhost:8000/lessons', {}, {withCredentials: true});
+  console.log(res);
+}
 
 function App() {
+  test();
   return (
     <Router>
-    <Switch>
-      <Route exact path="/" component={temp} />
-      <Route exact path="/route" component={temp} />
-      {/* add more routes here */}
-    </Switch>
-  </Router>
-
+      <Switch>
+        <Route exact path='/' component={temp} />
+        <Route exact path='/route' component={temp} />
+        {/* add more routes here */}
+      </Switch>
+    </Router>
   );
 }
 
