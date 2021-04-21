dbd53b3e8705834710b121c8759796345acf6e7b
Albert Zhang <aalbertzhang@gmail.com>
2021-04-20 18:59:16 -0700

    use https for backend url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  apiURL: process.env.NODE_ENV === 'development' ? "http://localhost:8000" : 'http://ccl-videos-backend.herokuapp.com',
+  apiURL: process.env.NODE_ENV === 'development' ? "http://localhost:8000" : 'https://ccl-videos-backend.herokuapp.com',
   internalURL: "http://localhost:3000",
   bucket: "ccl-videos",
 };
