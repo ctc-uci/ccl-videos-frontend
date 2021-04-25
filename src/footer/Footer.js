daf5184c952e6b5e2eb42de740d9dab61fe80c9d
Albert Zhang <aalbertzhang@gmail.com>
2021-04-24 20:19:27 -0700

    update footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,37 +1,55 @@
-import React from "react";
-import { Row, Col } from "shards-react";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import "footer/Footer.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import React from 'react';
+import { Row, Col } from 'shards-react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import 'footer/Footer.css';
 
 const Footer = () => {
-  library.add(fab)
+  library.add(fab);
   return (
-    <Row className="Footer1">
-      <Col className="socialMediaContainer" sm="12" md="4" lg="3">
-        <h2 className="footerTextBold">Child Creativty Lab</h2>
-        <a href="https://www.facebook.com/childcreativitylab/"><FontAwesomeIcon className="footerTextBold" icon={['fab', 'facebook']} /></a>
-        <a href="https://www.instagram.com/childcreativitylab/?hl=en"><FontAwesomeIcon className="footerTextBold" icon={['fab', 'instagram']} /></a>
-        <a href="https://www.youtube.com/user/Childcreativity/featured"><FontAwesomeIcon className="footerTextBold" icon={['fab', 'youtube']} /></a>
-        <a href="https://twitter.com/childcreativity?lang=en"><FontAwesomeIcon className="footerTextBold" icon={['fab', 'twitter']} /></a>
-        <h2 className="footerText">&#169; 2021 Child Creativty Labs</h2>
-      </Col>
-      <Col className="websiteContainer" sm="12" md="4" lg="3">
-        <a href="https://www.childcreativitylab.org/checkout/donate?donatePageId=5b1612e8352f539abbc6378f"><h2 className="footerTextBold">Donate</h2></a>
-        <a href="https://www.childcreativitylab.org/shop"><h2 className="footerTextBold">Store</h2></a>
-        <a href="https://www.childcreativitylab.org/about-us"><h2 className="footerTextBold">About Us</h2></a>
-        <a href="https://www.childcreativitylab.org/home"><h2 className="footerTextBold">Website</h2></a>
-      </Col>
-      <Col className="contactContainer" sm="12" md="4" lg="6">  
-        <div className="addressFooter">
-          <h2 className="footerText">1901 Carnegie Ave. Suite 1A</h2>
-          <h2 className="footerText">Santa Ana, CA 92705</h2>
-          <h2 className="footerText">(714) 352-4380</h2>
-          <h2 className="footerText">info@childcreativitylab.org</h2>
-        </div>
-      </Col>
-    </Row>
+    <div className='footer-container'>
+      <Row className="footer-row">
+        <Col className='footer-social-media-container' sm='12' md='4' lg='3'>
+          <h2 className='footer-text-bold'>Child Creativity Lab</h2>
+          <a href='https://www.facebook.com/childcreativitylab/'>
+            <FontAwesomeIcon className='footer-text-bold' icon={['fab', 'facebook']} />
+          </a>
+          <a href='https://www.instagram.com/childcreativitylab/?hl=en'>
+            <FontAwesomeIcon className='footer-text-bold' icon={['fab', 'instagram']} />
+          </a>
+          <a href='https://www.youtube.com/user/Childcreativity/featured'>
+            <FontAwesomeIcon className='footer-text-bold' icon={['fab', 'youtube']} />
+          </a>
+          <a href='https://twitter.com/childcreativity?lang=en'>
+            <FontAwesomeIcon className='footer-text-bold' icon={['fab', 'twitter']} />
+          </a>
+          <h2 className='footer-text'>&#169; 2021 Child Creativity Labs</h2>
+        </Col>
+        <Col className='footer-website-container' sm='12' md='4' lg='3'>
+          <a href='https://www.childcreativitylab.org/checkout/donate?donatePageId=5b1612e8352f539abbc6378f'>
+            <h2 className='footer-text-bold'>Donate</h2>
+          </a>
+          <a href='https://www.childcreativitylab.org/shop'>
+            <h2 className='footer-text-bold'>Store</h2>
+          </a>
+          <a href='https://www.childcreativitylab.org/about-us'>
+            <h2 className='footer-text-bold'>About Us</h2>
+          </a>
+          <a href='https://www.childcreativitylab.org/home'>
+            <h2 className='footer-text-bold'>Website</h2>
+          </a>
+        </Col>
+        <Col className='footer-contact-container' sm='12' md='4' lg='6'>
+          <div>
+            <h2 className='footer-text'>1901 Carnegie Ave. Suite 1A</h2>
+            <h2 className='footer-text'>Santa Ana, CA 92705</h2>
+            <h2 className='footer-text'>(714) 352-4380</h2>
+            <h2 className='footer-text'>info@childcreativitylab.org</h2>
+          </div>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
